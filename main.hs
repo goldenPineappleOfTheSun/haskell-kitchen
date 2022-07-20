f010dab975100695342f7e33bc395e858b4fa967
saetov <djcore@yandex.ru>
2022-07-21 01:38:50 +0500

    🍎. Добавь к холодильнику "морозильник". В который также можно ложить предметы. Добавь "полки" в которые также можно ложить предметы. Должно получиться три разных вида хранилищ и у всех ограничение на вместимость и одинаковый удобный интерфейс

--- main.hs
+++ main.hs
@@ -16,14 +16,24 @@ matchFood _ _ = False
 
 -- Storages --
 
-data Storage = Fridge {capacity :: Int, slots :: [Food]}
+data Storage = Fridge {capacity :: Int, slots :: [Food]} | Freezer {capacity :: Int, slots :: [Food]} | Shelve {capacity :: Int, slots :: [Food]}
 
 createFridge :: Int -> Storage
 createFridge x = Fridge {capacity = x, slots = []}
 
+createFreezer :: Int -> Storage
+createFreezer x = Freezer {capacity = x, slots = []}
+
+createShelve :: Int -> Storage
+createShelve x = Shelve {capacity = x, slots = []}
+
 putInStorage :: Storage -> [Food] -> Storage
 putInStorage fridge@(Fridge{capacity=c, slots=s}) [] = fridge
 putInStorage fridge@(Fridge{capacity=c, slots=s}) (x:xs) = if (length s < c) then putInStorage (Fridge {capacity = c, slots = x:s}) xs else fridge 
+putInStorage freezer@(Freezer{capacity=c, slots=s}) [] = freezer
+putInStorage freezer@(Freezer{capacity=c, slots=s}) (x:xs) = if (length s < c) then putInStorage (Freezer {capacity = c, slots = x:s}) xs else freezer 
+putInStorage shelve@(Shelve{capacity=c, slots=s}) [] = shelve
+putInStorage shelve@(Shelve{capacity=c, slots=s}) (x:xs) = if (length s < c) then putInStorage (Shelve {capacity = c, slots = x:s}) xs else shelve
 
 _findInStorage :: [Food] -> Food -> Food
 _findInStorage [] food = NoFood
@@ -31,9 +41,13 @@ _findInStorage (x:xs) food = if (matchFood x food) then x else (_findInStorage x
 
 findInStorage :: Storage -> Food -> Food
 findInStorage Fridge{slots=s} f = _findInStorage s f
+findInStorage Freezer{slots=s} f = _findInStorage s f
+findInStorage Shelve{slots=s} f = _findInStorage s f
 
 renderStorage :: Storage -> String
 renderStorage (Fridge c s) = "❄️" ++ show s
+renderStorage (Freezer c s) = "❄️" ++ show s
+renderStorage (Shelve c s) = "❄️" ++ show s
 
 -- Show instances --
 
@@ -56,5 +70,5 @@ main =  do
         apple_1 = Apple Normal
         apple_2 = Apple Normal
         mango_1 = Mango Normal
-        fridge = putInStorage (createFridge 2) [apple_1, apple_2, mango_1]
+        fridge = putInStorage (createShelve 2) [apple_1, apple_2, mango_1]
         got_apple = findInStorage fridge apple_1
